febf8ff9012f8f8ff9d31d6e41a4f9931c117747
Andy Davies <andy@mojotech.com>
2017-09-11 13:53:23 -0400

    v3 edits and to-do add+remove

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const answerAccordion = document.getElementById('answer-accordion')
-const answerButton = document.getElementById('answer-button')
+const answerAccordion = document.getElementById("answer-accordion")
+const answerButton = document.getElementById("answer-button")
+const toDoList = document.getElementById("to-do-list")
+const toDoListItems = Array.from(document.getElementsByClassName("list-item--to-do"))
+const completedToDoList = document.getElementById("completed-to-do-list")
 
 document.addEventListener("DOMContentLoaded",function(){
+
+  function removeToDo(){
+    toDoListItems.forEach(function(item, index){
+      item.addEventListener("click", function(){
+
+        let listItem = document.createElement("li")
+        let listItemContent = this.querySelector("label").textContent
+        listItem.append(listItemContent)
+        listItem.classList.add("list-item", "list-item--complete")
+        completedToDoList.append(listItem)
+
+        this.remove();
+      })
+    })
+  }
+
   answerButton.addEventListener("click", function(){
     answerAccordion.classList.toggle("open")
     answerButton.classList.toggle("open")
   })
+
+  removeToDo();
 });
